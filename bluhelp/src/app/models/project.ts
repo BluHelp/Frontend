3e8a33fd2d9542eb37baf5a9da58097761deb9a7
Gabriella <gabimabento@gmail.com>
2022-10-12 19:00:08 -0300

    Criado Service e Model do Projeto

---
+++ bluhelp/src/app/models/project.ts
@@ -0,0 +1,11 @@
+export interface Project {
+    id: number;
+    creator: number;
+    title: string;
+    objective: string;
+    address: number;
+    description: string;
+    categories: number[];
+    photo: string;
+    
+}
---
+++ bluhelp/src/app/services/project.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Project } from '../models/project';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectService {
+
+  private endpoint = 'http://localhost:8080/project/'
+
+  constructor(private client: HttpClient) { }
+
+  addProject(project: Project): Observable<Project>{
+		return this.client.post<Project>(this.endpoint, project);
+	}
+}
