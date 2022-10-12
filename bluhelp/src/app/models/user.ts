d7b856cf4fa5f08a6f951c52bca3edfa46a8c816
Gabriella <gabimabento@gmail.com>
2022-10-12 00:07:19 -0300

    Atualizado Model User

--- bluhelp/src/app/models/user.ts
+++ bluhelp/src/app/models/user.ts
@@ -1,2 +1,12 @@
 export interface User {
+    id: number;
+    name: string;
+    surname: string;
+    password: string;
+    cpf: string;
+    photo: string;
+    email: string;
+    phone: string;
 }
+
+
