7039e283a9177294d91e5c5be74196b2bf0cab53
Daniel Barth <Daniel.renato.barth@gmail.com>
2022-10-14 14:25:06 -0300

    Criado a rota (HOME)

--- bluhelp/src/app/app-routing.module.ts
+++ bluhelp/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProjectFormComponent } from './components/project-form/project-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "home", component : HomeComponent}c
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- bluhelp/src/app/app.module.ts
+++ bluhelp/src/app/app.module.ts
@@ -7,12 +7,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
 import { ProjectFormComponent } from './components/project-form/project-form.component';
+<<<<<<< Updated upstream
+=======
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< Updated upstream
     UserComponent,
     ProjectFormComponent
+=======
+    UserFormComponent,
+    ProjectFormComponent,
+    UserProfileComponent,
+    HeaderComponent,
+    NavbarComponent,
+    HomeComponent
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
