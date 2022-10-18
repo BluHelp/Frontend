ec98cdeb99edf7d6a9a29706997104a9997c487f
Gabriella <gabimabento@gmail.com>
2022-10-17 21:31:42 -0300

    Atualização e alteração nas Rotas

--- bluhelp/src/app/app-routing.module.ts
+++ bluhelp/src/app/app-routing.module.ts
@@ -9,15 +9,17 @@ import { HomeComponent } from './components/home/home.component';
 import { ProjectFormComponent } from './components/project-form/project-form.component';
 import { LoginComponent } from './components/login/login.component';
 import { UserFormComponent } from './components/user-form/user-form.component';
+import { ProjectProfileComponent } from './components/project-profile/project-profile.component';
 
 const routes: Routes = [
 
   { path: "", component: LoginComponent },
-  { path: "Cadastro", component: UserFormComponent },
+  { path: "Register", component: UserFormComponent },
   { path: "Home/:id", component: HomeComponent },
   { path: "Search", component: SearchComponent },
-  { path: "criarProjeto/:id", component: ProjectFormComponent },
+  { path: "CreateProject/:id", component: ProjectFormComponent },
   { path: "Profile", component: UserProfileComponent },
+  { path: "ProjectProfile", component: ProjectProfileComponent},
   { path: "exitSystem", component: ExitSystemComponent }
 ];
 
