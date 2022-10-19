def0d707e0d51a37edfe3573a3e80e139ad4c1fe
Gabriella <gabimabento@gmail.com>
2022-10-19 13:46:36 -0300

    Atualizado Componente Contributions

--- bluhelp/src/app/app-routing.module.ts
+++ bluhelp/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { ProjectFormComponent } from './components/project-form/project-form.com
 import { LoginComponent } from './components/login/login.component';
 import { UserFormComponent } from './components/user-form/user-form.component';
 import { ProjectProfileComponent } from './components/project-profile/project-profile.component';
+import { ContributionsComponent } from './components/contributions/contributions.component';
 
 const routes: Routes = [
 
@@ -19,8 +20,9 @@ const routes: Routes = [
   { path: "search", component: SearchComponent },
   { path: "create-project/:id", component: ProjectFormComponent },
   { path: "profile", component: UserProfileComponent },
-  { path: "project-profile", component: ProjectProfileComponent},
-  { path: "exit-system", component: ExitSystemComponent }
+  { path: "project-profile/:idproject", component: ProjectProfileComponent},
+  { path: "exit-system", component: ExitSystemComponent },
+  { path: "contributions/:id", component: ContributionsComponent}
 ];
 
 @NgModule({
